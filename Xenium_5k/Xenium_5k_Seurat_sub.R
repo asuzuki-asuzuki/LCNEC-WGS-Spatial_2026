41fdc9949e2a007b546a68bd2d958f1f687c9de7
asuzuki-asuzuki <110817918+asuzuki-asuzuki@users.noreply.github.com>
2026-06-27 18:14:20 +0900

    Add files via upload

---
+++ Xenium_5k/Xenium_5k_Seurat_sub.R
@@ -0,0 +1,24 @@
+library(Seurat) # v5.4.0
+library(dplyr)
+library(patchwork)
+library(ggplot2)
+
+options(future.globals.maxSize = 100 * 1024^3) # 100 Gb
+set.seed(1234)
+
+packageVersion("Seurat")
+
+# Load a merged Seurat object
+lung <- readRDS("lung_xenium_merge_sketch_ProjectData_annotation.rds")
+
+# extraction of epitehlial cell sub-clusters
+lung <- subset(lung, subset = merge_annotation %in% c("Epithelial cell, NE tumor", "Epithelial cell, non-NE tumor", "Epithelial cell, basal", "Epithelial cell, bronchiolar", "Epithelial cell, alveolar"))
+
+DefaultAssay(lung) <- "Xenium"
+lung
+
+# Save the object
+saveRDS(lung, file = "lung_sub.rds")
+
+rm(list = ls())
+gc();gc()
